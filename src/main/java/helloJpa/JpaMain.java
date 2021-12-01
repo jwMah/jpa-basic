03874707bfc589d97aa0805659f5ec71fd8278e9
jwMah <wigbuldok@naver.com>
2021-12-01 20:08:35 +0900

    init

---
+++ src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,38 @@
+package helloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("jaewan");
+            em.persist(member);
+            em.flush();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            
+            //findMember.setName("helloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
